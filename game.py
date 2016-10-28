bec21417c09bb529881b1fea486073db65948b4d
U-Ted-PC\Ted <wong.ted.t@gmail.com>
2016-10-28 12:51:10 -0400

    Fixed dancers swapping locations

--- game.py
+++ game.py
@@ -260,10 +260,10 @@ def game_finished(board, red, blue):
 
 def print_board():
 	# clear the screen to keep the board be displayed at the same place
-#	cmd = "clear"
-#	if sys.platform == "win32":
-#		cmd = "cls"
-#	os.system(cmd)
+	cmd = "clear"
+	if sys.platform == "win32":
+		cmd = "cls"
+	os.system(cmd)
 	
 	for r in board:
 		for c in r:
